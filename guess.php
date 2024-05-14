5e8b4ce55c0572c20eb5a4010aa1b246d819b482
Saudtahi <108763206+Saudtahi@users.noreply.github.com>
2024-05-14 14:48:43 +0500

    guess.php

---
+++ guess.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>0839d06f - Guessing Game</title>
+</head>
+<body>
+    <h1>Welcome to the Guessing Game</h1>
+
+    <?php
+    $correct_number = 55;
+    $guess = isset($_GET['guess']) ? $_GET['guess'] : null;
+    $message = '';
+
+    if (isset($guess)) {
+        if (!is_numeric($guess)) {
+            $message = "Your guess is not a number.";
+        } else {
+            if ($guess < $correct_number) {
+                $message = "Your guess is too low.";
+            } elseif ($guess > $correct_number) {
+                $message = "Your guess is too high.";
+            } else {
+                $message = "Congratulations - You are right!";
+            }
+        }
+    }
+    ?>
+
+    <form method="get">
+        <p><label for="guess">Enter your guess:</label>
+        <input type="text" name="guess" id="guess" size="60"></p>
+        <input type="submit">
+    </form>
+
+    <p><?php echo htmlentities($message); ?></p>
+</body>
+</html>
