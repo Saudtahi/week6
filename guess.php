302561d897ee6f8431a6a2d0964fd3966dad5aef
Saudtahi <108763206+Saudtahi@users.noreply.github.com>
2024-05-14 14:57:52 +0500

    guess.php

--- guess.php
+++ guess.php
@@ -1,26 +1,34 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>0839d06f - Guessing Game by Saud</title>
+    <title>0839d06f</title>
 </head>
 <body>
     <h1>Welcome to the Guessing Game</h1>
 
     <?php
     $correct_number = 13;
-    $guess = isset($_GET['guess']) ? $_GET['guess'] : null;
     $message = '';
 
-    if (isset($guess)) {
-        if (!is_numeric($guess)) {
-            $message = "Your guess is not a number.";
+    // Check if the guess parameter is set
+    if (isset($_GET['guess'])) {
+        $guess = $_GET['guess'];
+
+        // Validate the guess parameter
+        if (strlen($guess) < 1) {
+            $message = "Missing guess parameter";
+        } elseif (strlen($guess) < 3) { // Assuming 'too short' means less than 3 characters
+            $message = "Your guess is too short";
+        } elseif (!is_numeric($guess)) {
+            $message = "Your guess is not a number";
         } else {
+            $guess = (int) $guess;
             if ($guess < $correct_number) {
-                $message = "Your guess is too low.";
+                $message = "Your guess is too low";
             } elseif ($guess > $correct_number) {
-                $message = "Your guess is too high.";
+                $message = "Your guess is too high";
             } else {
-                $message = "Congratulations - You are right!";
+                $message = "Congratulations - You are right";
             }
         }
     }
